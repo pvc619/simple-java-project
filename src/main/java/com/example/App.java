d4c2f24223e95b52f9d52bf6be5e1722bfdef4e1
pvc619 <pvc619@gmail.com>
2024-05-28 17:44:15 +0530

    2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, Jenkins 5!!");
 
-=======
         System.out.println("Hello, Jenkins 5!!");
->>>>>>> 6c5db6f22a4ff07b5a0f761ead8f665246eca402
     }
 }
 
